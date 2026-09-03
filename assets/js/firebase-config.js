38998890d57f9c158524c53bb1f965e61c3e162f
Matheus Vinicius <matheusviniciuscaieiras@gmail.com>
2026-09-03 16:52:24 -0300

    Configura credenciais reais do Firebase (projeto auxiliar-b0410)

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -3,10 +3,10 @@
 // ative "Authentication > Email/Password" e "Firestore Database",
 // depois cole aqui as credenciais de "Configurações do projeto > Seus apps > SDK".
 export const firebaseConfig = {
-    apiKey: "COLE_SUA_API_KEY_AQUI",
-    authDomain: "SEU_PROJETO.firebaseapp.com",
-    projectId: "SEU_PROJETO",
-    storageBucket: "SEU_PROJETO.appspot.com",
-    messagingSenderId: "SEU_SENDER_ID",
-    appId: "SEU_APP_ID"
+    apiKey: "AIzaSyAy5bj4C_m5P6DkRMBuqYS7EaDec4_isUI",
+    authDomain: "auxiliar-b0410.firebaseapp.com",
+    projectId: "auxiliar-b0410",
+    storageBucket: "auxiliar-b0410.firebasestorage.app",
+    messagingSenderId: "527244083559",
+    appId: "1:527244083559:web:55e93e79205df1a79e6fc2"
 };
